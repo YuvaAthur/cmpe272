877d9ec6d5d91a92438978082b2cb9e648fabb26
Yuva Athur <yuvaraj.a.r@gmail.com>
2019-05-05 21:57:13 -0700

    changes in test_rest_api file

--- BookStore/bookstore/webapi/trial/test_rest_api.py
+++ BookStore/bookstore/webapi/trial/test_rest_api.py
@@ -14,8 +14,8 @@ mongo = pymongo.MongoClient('mongodb+srv://warriors:warriors@warriors-vc2w4.mong
 
 db = pymongo.database.Database(mongo,'warriors')
 book = pymongo.collection.Collection(db,'books')
-order = pymongo.collection.Collection(db,'orders')
-customer = pymongo.collection.Collection(db,'customers')
+orders = pymongo.collection.Collection(db,'orders')
+customers = pymongo.collection.Collection(db,'customers')
 
 mongo = PyMongo(app)
 
@@ -39,20 +39,45 @@ def get_one_book(isbn):
 
     return jsonify({'result' : output})
 
+@app.route('/orders', methods=['GET'])
+def get_all_orders():
+    return jsonify({'result' : json.loads(dumps(order.find().limit(5).sort("time", -1)))})
+
 @app.route('/create_order', methods=['POST'])
 def add_order():
 
     isbn = request.json['isbn']
     quantity = request.json['quantity']
     customer = request.json['Customer_id']
-    order_id = db.orders.find({},{order_id:1, _id: 0}).sort({order_id:-1} ).limit(1)+1
+    order_id = orders.find({},{order_id:1, _id: 0}).sort({order_id:-1} ).limit(1)+1
 
-    #if isbn in db.books.find( { 'isbn': "isbn" }, { isbn: 1, _id: 0 } ) and quantity <= db.books.find( { 'isbn': "isbn" }, { quantity: 1, _id: 0 } ) and customer in db.customers.find( { 'Customer_id': "customer
-" }, { Customer_id: 1, _id: 0 } )
+if isbn not in book.find( { 'isbn': "isbn" }, { isbn: 1, _id: 0 } ):
+        print("ISBN used is not valid")
+elif  quantity > book.find( { 'isbn': "isbn" }, { quantity: 1, _id: 0 } ):
+       print("Quantity is more than available copies. Update your quantity to be less than or equal to available copies")
+elif customer in customers.find({'Customer_id':"customer"},{Customer_id: 1, _id:0}):
+      print("Balance is below 0, add funds now or you will be charged a penalty.")
+else:
+    orders.insert({'order_id' : order_id, 'isbn' : isbn, 'quantity' : quantity, 'Customer_id' : Customer_id, 'Status': "Created" })
+    print(order_id)
+#return jsonify({'Order ID' : order_id})
 
-    db.orders.insert({'order_id' : order_id, 'isbn' : isbn, 'quantity' : quantity, 'Customer_id' : Customer_id, 'Status': "Created" })
+@app.route('/order_fulfill/<order_id>', methods=['PUT'])
+def fulfill_order(order_id):
 
-    return jsonify({'Order ID' : order_id})
+    q = orders.find({'order_id': order_id,'Status' : "Created"}, {"Status":1, _id: 0 } )
+    isbn = orders.find({'order_id': order_id,'Status' : "Created"}, {"isbn":1, _id: 0 } )
+    current_qty = book.find({'isbn': isbn}, {"quantity":1, _id: 0 } )
+    order_qty = orders.find({'order_id': order_id,'Status' : "Created"}, {"quantity":1, _id: 0 } )
+    final_qty = current_qty['quantity'] - order_qty['quantity']
+
+    if q:
+        orders.updateOne({'order_id':order_id},{'Status':"Fulfilled"})
+        book.updateone({'isbn': isbn'},{'quantity': final_qty})
+    else:
+        output = 'Order is either fulfilled or not found'
+
+    return jsonify({'result' : output})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
